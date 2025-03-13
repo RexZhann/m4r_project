dabb79e19d41a9dd882ed0da28d5e58defe30df1
RexZhann <106326425+RexZhann@users.noreply.github.com>
2025-03-13 13:08:41 +0000

    structual mod

--- fest/scripts/conv_while.py
+++ fest/scripts/conv_while.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from firedrake import *
-from fest import refine_spacetime_alternately, refine_timespace_alternately
+from fest import refine_spacetime_alternately, refine_timespace_alternately, refine_timespace_with_convergence
 
-mu = 0.000001
+mu = 0.0001
 
 def bc_expr(x):
     return cos(2*pi*x)
@@ -17,13 +17,12 @@ def L(u, v):
 def exact(x, t):
     return cos(2*pi*x)*exp(-4*(pi**2) * mu * t)
 
-deg = 2
+deg = 1
 
-
-sp_hist, t_hist, err_hist = refine_spacetime_alternately(
+sp_hist, t_hist, err_hist = refine_timespace_alternately(
     sp_init=10,
     t_init=10,
-    t_end=1,
+    t_end=0.5,
     deg=deg,
     bc_expr=bc_expr,
     h=h,
@@ -31,13 +30,13 @@ sp_hist, t_hist, err_hist = refine_spacetime_alternately(
     exact=exact,
     num_iter_mode='step',  
     outer_tol=1e-10,
-    max_iter=4,
+    max_iter=8,
     return_inner=False
 )
 
 
 fig, ax = plt.subplots(1, 1, figsize=(7, 5))
-ax.loglog([1/n for n in t_hist], err_hist, marker='o')
+ax.loglog([1/n for n in sp_hist], err_hist, marker='o')
 ax.set_xlabel("dt")
 ax.set_ylabel("Error (log scale)")
 ax.set_title(f"Alternating refinement of space/time until error < 1e-12(deg {deg})")
